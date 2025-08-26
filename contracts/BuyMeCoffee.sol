7a81bef2a26d63d8ea4f1d6defac97ba8b93ab95
Vaibhav <vaibhavritam@gmail.com>
2025-08-26 17:33:06 +0800

    Reusing Foundry_Fund_Me for contract

--- contracts/BuyMeCoffee.sol
+++ contracts/BuyMeCoffee.sol
@@ -1,46 +1,46 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+// // SPDX-License-Identifier: MIT
+// pragma solidity 0.8.19;
 
-contract BuyMeCoffee {
-    /* Errors */
-    error BuyMeCoffee__OnlyOwnerAccessible();
-    error BuyMeCoffee__FundWithoutEth();
-    error BuyMeCoffee__NoFundsToWithdraw();
-    error BuyMeCoffee__ErrorWithdrawingFunds();
+// contract BuyMeCoffee {
+//     /* Errors */
+//     error BuyMeCoffee__OnlyOwnerAccessible();
+//     error BuyMeCoffee__FundWithoutEth();
+//     error BuyMeCoffee__NoFundsToWithdraw();
+//     error BuyMeCoffee__ErrorWithdrawingFunds();
 
-    /* State Variables */
-    address private immutable i_owner;
+//     /* State Variables */
+//     address private immutable i_owner;
 
-    address[] private s_funders;
+//     address[] private s_funders;
 
-    constructor(address owner) {
-        i_owner = owner;
-    }
+//     constructor(address owner) {
+//         i_owner = owner;
+//     }
 
-    function buyCoffee() external payable {
-        if (msg.value == 0) {
-            revert BuyMeCoffee__FundWithoutEth();
-        }
+//     function buyCoffee() external payable {
+//         if (msg.value == 0) {
+//             revert BuyMeCoffee__FundWithoutEth();
+//         }
 
-        s_funders.push(msg.sender);
-    }
+//         s_funders.push(msg.sender);
+//     }
 
-    function withdrawCoffeeFunds() external onlyOwner {
-        if (address(this).balance == 0) {
-            revert BuyMeCoffee__NoFundsToWithdraw();
-        }
+//     function withdrawCoffeeFunds() external onlyOwner {
+//         if (address(this).balance == 0) {
+//             revert BuyMeCoffee__NoFundsToWithdraw();
+//         }
 
-        (bool success, ) = i_owner.call{value: address(this).balance}("");
+//         (bool success, ) = i_owner.call{value: address(this).balance}("");
 
-        if(!success){
-            revert BuyMeCoffee__ErrorWithdrawingFunds();
-        }
-    }
+//         if(!success){
+//             revert BuyMeCoffee__ErrorWithdrawingFunds();
+//         }
+//     }
 
-    modifier onlyOwner() {
-        if (msg.sender != i_owner) {
-            revert BuyMeCoffee__OnlyOwnerAccessible();
-        }
-        _;
-    }
-}
+//     modifier onlyOwner() {
+//         if (msg.sender != i_owner) {
+//             revert BuyMeCoffee__OnlyOwnerAccessible();
+//         }
+//         _;
+//     }
+// }
